9b2604c4bec2c251148e6880eea103090f21f65c
CamiloCanonM <camican1984@gmail.com>
2025-12-15 16:26:09 -0500

    app.py

--- chatbotV2/app.py
+++ chatbotV2/app.py
@@ -33,7 +33,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-col1, col2, col3 = st.columns([1, 5, 1]) 
+col1, col2, col3 = st.columns([1, 8, 1]) 
 
 with col2:
     # Truco: Obtenemos la ruta exacta de donde está este archivo app.py
@@ -431,6 +431,7 @@ if prompt_usuario:
         
         st.session_state.chat_history.append(AIMessage(content=respuesta_ia))
         if es_vision: st.rerun()
+
 
 
 
